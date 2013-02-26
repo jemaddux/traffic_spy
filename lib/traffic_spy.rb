5ed5e07b37daf79c7840aca689994c073bfe2f03
John Maddux <jemaddux@gmail.com>
2013-02-26 16:51:14 -0700

    Another test working

--- lib/traffic_spy.rb
+++ lib/traffic_spy.rb
@@ -1,8 +1,13 @@
 require "traffic_spy/version"
 require 'sinatra'
 
-get '/sources' do
-  'Hello world!'
+post '/sources' do
+  if params[:rootUrl].nil?
+    status 400
+    "{\"message\":\"no url provided\"}"
+  else
+    "{\"identifier\":\"#{params[:identifier]}\"}"
+  end
 end
 
 module TrafficSpy
--- spec/traffic_spy_spec.rb
+++ spec/traffic_spy_spec.rb
@@ -11,10 +11,25 @@ describe 'The Trafic Spy App' do
     Sinatra::Application
   end
 
-  it "says hello" do
-    get '/sources'
-    last_response.should be_ok
-    #last_response.body.should == 'Hello World'
+  describe "POST /sources" do
+    context "with both identifier and rootUrl" do
+      it "returns a 200(OK) with a body" do
+        post "/sources", :identifier => "jumpstartlab", :rootUrl => 'http://jumpstartlab.com'
+
+        last_response.should be_ok
+        last_response.body.should eq "{\"identifier\":\"jumpstartlab\"}"
+      end
+    end
+
+    context "with indentifier but without rootURL" do
+      it "returns 400 with an error meesage" do
+        post "/sources", :identifier => "jumpstartlab"
+        last_response.status.should eq 400
+        last_response.body.should eq "{\"message\":\"no url provided\"}"
+      end
+    end
   end
+
+
 
 end
